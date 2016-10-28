406933bca04616f4098d087c7ee4788db8763cb9
Patrick Brockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-10-28 18:42:43 +0200

    Pip refactoring

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from distutils.core import setup
 setup(
   name = 'ipython_ferretmagic',
   packages = ['ipython_ferretmagic'],
-  install_requires=['ipython'],
-  py_modules=['ipython_ferretmagic'],
+  py_modules = ['ipython_ferretmagic'],
   version = '2016.10',
   description = 'ipython extension for pyferret',
   author = 'Patrick Brockmann',
   author_email = 'Patrick.Brockmann@lsce.ipsl.fr',
-  url = 'https://github.com/PBrockmann/ipython_ferretmagic', 
+  url = 'https://github.com/PBrockmann/ipython_ferretmagic',
   download_url = 'https://github.com/PBrockmann/ipython_ferretmagic/tarball/master',
-  keywords = ['ipython', 'jupyter', 'ferret', 'pyferret', 'magic', 'extension'], 
-  classifiers = [],
+  keywords = ['jupyter', 'ipython', 'ferret', 'pyferret', 'magic', 'extension'],
+  classifiers = []
 )
