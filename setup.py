6dc20285f0be87c7e16ba0a4619be6e1c030c1d8
Patrick Brockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-10-28 17:10:46 +0200

    PyPi'd

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'ipython_ferretmagic',
+  packages = ['ipython_ferretmagic'],
+  version = '',
+  description = 'ipython notebook extension for pyferret',
+  author = 'Patrick Brockmann',
+  author_email = 'Patrick.Brockmann@lsce.ipsl.fr',
+  url = 'https://github.com/PBrockmann/ipython_ferretmagic', 
+  download_url = 'https://github.com/PBrockmann/ipython_ferretmagic/tarball/7.0.0',
+  keywords = ['ipython', 'jupyter', 'ferret', 'pyferret', 'magic', 'extension'], 
+  classifiers = [],
+)
