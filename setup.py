b5c62074b0fc78343b2776a1e557a34fb8efcba3
Patrick Brockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-10-28 17:57:06 +0200

    Fix

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
   name = 'ipython_ferretmagic',
   packages = ['ipython_ferretmagic'],
-  version = '',
-  description = 'ipython notebook extension for pyferret',
+  version = '2016.10',
+  description = 'ipython extension for pyferret',
+  long_description=open('README.md').read(),
   author = 'Patrick Brockmann',
   author_email = 'Patrick.Brockmann@lsce.ipsl.fr',
   url = 'https://github.com/PBrockmann/ipython_ferretmagic', 
-  download_url = 'https://github.com/PBrockmann/ipython_ferretmagic/tarball/7.0.0',
+  download_url = 'https://github.com/PBrockmann/ipython_ferretmagic/tarball/master',
   keywords = ['ipython', 'jupyter', 'ferret', 'pyferret', 'magic', 'extension'], 
   classifiers = [],
 )
